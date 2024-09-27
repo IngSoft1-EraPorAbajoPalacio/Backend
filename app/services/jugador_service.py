f877c0396c60dd1124fe948e813cf34a3f43a5ea
lucas <lucas.gallegos@mi.unc.edu.ar>
2024-09-27 15:55:43 -0300

    SCRUM43_modelo_datos funcionales que modifican la base de datos relacionado al jugador

--- app/services/jugador_service.py
+++ app/services/jugador_service.py
@@ -1,20 +1,75 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import NoResultFound
-from db.models import Partida, engine
-from typing import List
+from db.base import SessionLocal
+from db.models import Jugador
+from sqlalchemy.exc import *
+from sqlalchemy.orm import Session
+from schema.jugador_schema import JugadorOut, JugadorIn
 
-# Crea una sesión
-Session = sessionmaker(bind=engine)
+#jugadores
 
-class JugadorBD:
-
-    def obtener_cartas_movimientos():
-        return None
-
-    def obtener_cartas_figura():
-        return None
-
-    def  get_tablero():
-        return None
+def crear_jugador(db:Session , jugador : JugadorIn) :
+    jugador_creado = Jugador(nickname=jugador.nickname)
+    try :
+        db.add(jugador_creado)
+        db.commit()
+        return JugadorOut(id= jugador_creado.id, nickname=jugador_creado.nickname)
+    except SQLAlchemyError as e :
+        print(str(e))
 
     
+def obtener_jugadores_db(db:Session):
+    try : 
+        jugadores = db.query(Jugador).all()
+        return [JugadorOut(id=jugador.id, nickname=jugador.nickname) for jugador in jugadores]            
+    except Exception as e:
+        print(f'Ocurrió una excepción {e}')
+
+
+def obtener_jugador_particular(id:int, db : Session):
+    jugador = db.query(Jugador).filter(Jugador.id == id).first()
+    if jugador is None:
+        return {f"No existe jugador con id {id}"}
+    return jugador
+
+
+def eliminar_jugador_db(id: int, db:Session) :
+    try :
+        jugador = db.query(Jugador).filter(Jugador.id == id).first()
+        db.delete(jugador)
+        db.commit()
+        return JugadorOut(id=jugador.id, nickname=jugador.nickname)
+    except Exception as e:
+        print(f"Ocurrió una excepción {e}")
+
+
+def obtener_cartas_movimientos():
+    return None
+def obtener_cartas_figura():
+    return None
+def  get_tablero():
+    return None
+
+
+
+"""
+
+def agregar_jugador(self,nickname : str) -> JugadorOut :
+    session = SessionLocal()
+    jugador_creado = Jugador(nickname=nickname)
+    try:
+        session.add(jugador_creado)
+        session.commit()
+            return JugadorOut(id= jugador_creado.id, nickname=jugador_creado.nickname)
+        except SQLAlchemyError as e :
+            print(str(e))
+        finally:
+            session.close()            
+         
+    def obtener_partidas_de_jugador(self,id) :
+        session = SessionLocal()
+        jugador = self.obtener_jugador_particular(id)
+        for jugador_partida in jugador.relacion_JugadorPartida:
+            print(jugador_partida)        
+
+
+
+"""
