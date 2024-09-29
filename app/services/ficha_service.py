be272812315f1e6a6b1c7dee6839c441cd382720
lucas <lucas.gallegos@mi.unc.edu.ar>
2024-09-29 15:10:25 -0300

    service fichas

---
+++ app/services/ficha_service.py
@@ -0,0 +1,29 @@
+from sqlalchemy.exc import *
+import random
+from sqlalchemy.orm import Session
+from db.models import *
+
+TAMANO_TABLERO = 6
+
+def crear_tablero(id_partida: int, db: Session):       
+        partida = db.query(Partida).filter(Partida.id == id_partida).first()
+        id_jugadores = [jugador.id_jugador for jugador in partida.jugadores]        
+        tablero_nuevo = Tablero(id_partida = id_partida, color_prohibido=random.choice(list(Color)),
+                                turno = random.choice(id_jugadores) )
+        db.add(tablero_nuevo)
+        db.commit()
+        
+        
+def repartir_fichas(id_partida : int , db:Session)  :
+    posiciones = []
+    for x in range(TAMANO_TABLERO):
+        for y in range(TAMANO_TABLERO):
+            posiciones.append(Ficha(x=x, y=y,color= random.choice(list(Color)),id_tablero = id_partida))            
+    db.add_all(posiciones)
+    db.commit()
+    
+
+
+                
+                
+            
