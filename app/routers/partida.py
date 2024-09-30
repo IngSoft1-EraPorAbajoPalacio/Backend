7d560a633ce8ac24d3ee619478446b31a47c4f42
lucas <lucas.gallegos@mi.unc.edu.ar>
2024-09-30 16:50:45 -0300

    ING-SCRUM-37 obtener cartas de figuras

--- app/routers/partida.py
+++ app/routers/partida.py
@@ -19,15 +19,15 @@ def obtener_partida(id: int, db: Session = Depends(crear_session)):
 async def crear_partida(partida: CrearPartida, db: Session = Depends(crear_session)):
     try:
         response = partida_service.crear_partida(partida, db)
-        await manager.broadcast({
-            "type": "AgregarPartida",
-            "data": {
-                "idPartida": response.id_partida,
-                "nombrePartida": partida.nombre_partida,
-                "cantJugadoresMin": partida.cant_min_jugadores,
-                "cantJugadoresMax": partida.cant_max_jugadores
-            }
-        })
+        #await manager.broadcast({
+        #    "type": "AgregarPartida",
+        #    "data": {
+        #        "idPartida": response.id_partida,
+        #        "nombrePartida": partida.nombre_partida,
+        #        "cantJugadoresMin": partida.cant_min_jugadores,
+        #        "cantJugadoresMax": partida.cant_max_jugadores
+        #    }
+        #})
         return response
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -37,37 +37,34 @@ async def unirse_partida(idPartida: str, request: UnirsePartidaRequest, db: Sess
     try:
         response = await partida_service.unirse_partida(idPartida, request.nombreJugador, db)
         jugadores = partida_service.obtener_jugadores(int(idPartida), db)
-        await manager.broadcast({
-            "type":"JugadorUnido",
-            "data":{
-                "idJugador": response.idJugador,
-                "ListaJugadores": [[j.id, j.nickname] for j in jugadores]
-            }
-        })
+        #await manager.broadcast({
+        #    "type":"JugadorUnido",
+        #    "data":{
+        #        "idJugador": response.idJugador,
+        #        "ListaJugadores": [[j.id, j.nickname] for j in jugadores]
+        #    }
+        #})
         return response
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
 
 @router.get("/partidas", response_model=List[PartidaResponse])
 async def listar_partidas(db: Session = Depends(crear_session)):
     try:
         return partida_service.listar_partidas(db)
     except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        raise HTTPException(status_code=404, detail= str(e))
 
-@router.post("/partida/{id_partida}/jugador/{id_jugador}", response_model=InicarPartidaResponse)
+
+@router.post("/partida/{id_partida}/jugador/{id_jugador}", status_code=201) #, response_model=InicarPartidaResponse)
 async def iniciar_partida(id_partida: int, id_jugador: int, db: Session = Depends(crear_session)):
     try:
         response = partida_service.iniciar_partida(id_partida, id_jugador, db)
         # Aquí deberías obtener la información necesaria para el mensaje de IniciarPartida
         # Este es un ejemplo, ajusta según tu lógica de juego
-        await manager.iniciar_partida(str(id_partida), {
-            "fichas": [{"x": 0, "y": 0, "color": "red"}],  # Ejemplo, ajusta según tu lógica
-            "orden": [1, 2, 3, 4],  # Ejemplo, ajusta según tu lógica
-            "cartasMovimiento": [{"id": 1, "movimiento": "adelante"}],  # Ejemplo
-            "cartasFigura": [{"idJugador": 1, "nombreJugador": "Jugador1", "cartas": [{"id": 1, "figura": 1}]}]  # Ejemplo
-        })
-        return response
+        await manager.broadcast(response)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
     
@@ -85,18 +82,18 @@ async def pasar_turno(id_partida: int, db: Session = Depends(crear_session)):
 @router.delete("/partida/{idPartida}/jugador/{idJugador}")
 async def abandonar_partida(id_partida: int, id_jugador: int, db: Session = Depends(crear_session)):
     try:
-        partida_service.abandonar_partida(id_partida, id_jugador, db)
-        jugadores = partida_service.obtener_jugadores(id_partida, db)
-        if len(jugadores) > 1:
-            await manager.eliminar_jugador(str(id_partida), {
-                "idJugador": id_jugador,
-                "ListaJugadores": [[j.id, j.nickname] for j in jugadores]
-            })
-        else:
-            await manager.terminar_partida(str(id_partida), {
-                "nombreJugGanador": jugadores[0].nickname
-            })
-        return {"message": "Jugador eliminado exitosamente"}
+        return partida_service.abandonar_partida(id_partida, id_jugador, db)
+        #jugadores = partida_service.obtener_jugadores(id_partida, db)
+        #if len(jugadores) > 1:
+        #    await manager.eliminar_jugador(str(id_partida), {
+        #        "idJugador": id_jugador,
+        #        "ListaJugadores": [[j.id, j.nickname] for j in jugadores]
+        #    })
+        #else:
+        #    await manager.terminar_partida(str(id_partida), {
+        #        "nombreJugGanador": jugadores[0].nickname
+        #    })
+        #return {"message": "Jugador eliminado exitosamente"}
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
 
