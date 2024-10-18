9c9ddb0e66d218b94b354612f559befe97b74e7d
lucas <lucas.gallegos@mi.unc.edu.ar>
2024-10-18 17:46:56 -0300

    SCURUM-95-Deshacer-Movimiento nuevo test para deshacer movimientos

--- app/tests/test_deshacer_movimiento.py
+++ app/tests/test_deshacer_movimiento.py
@@ -17,10 +17,17 @@ Session = sessionmaker(bind=engine)
 
 client = TestClient(app)
 
-Base.metadata.create_all(bind=engine) # Crea todas las tablas
+Base.metadata.drop_all(bind=engine) 
+Base.metadata.create_all(bind=engine) 
 
-db = Session()
-
+@pytest.fixture
+def crear_session():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()  
+    
 def movimiento_1(id_movimiento: int):
     return JugarMovimientoRequest(
         idCarta=id_movimiento,
@@ -80,7 +87,7 @@ def movimiento_7(id_movimiento: int):
         idCarta= id_movimiento ,
         posiciones=[
             Posicion(x=0, y=0),
-            Posicion(x=3, y=1)
+            Posicion(x=4, y=0)
         ]
     )
 
@@ -98,22 +105,23 @@ def switch_case(movimiento: int, id_movimiento):
 
 
 @pytest.mark.asyncio
-async def test_deshacer_movimiento():
+async def test_deshacer_movimiento(crear_session):
 
     datos_partida = CrearPartida(  
-        nombre_host = "lucas",
-        nombre_partida = "partida_test",
+        nombre_host = "owner1",
+        nombre_partida = "partida_test_1",
         cant_min_jugadores = 2,
         cant_max_jugadores = 4
      )
     
-    partida_creada = await partida_service.crear_partida(datos_partida, db)
-    jugador_unido1 = await partida_service.unirse_partida("1", "martin", db)
-    jugador_unido2 = await partida_service.unirse_partida("1", "mateo", db)
+    db = crear_session
+    
+    await partida_service.crear_partida(datos_partida, db)
+    await partida_service.unirse_partida("1", "j1", db)
+    await partida_service.unirse_partida("1", "j2", db)
     partida_iniciada = await partida_service.iniciar_partida(1, 1, db)
     
     tablero_inicial = partida_iniciada["fichas"]
-    print(f"el tablero inicial es de la forma: {tablero_inicial}")
     
     id_movimientos_en_mano = obtener_id_movimientos_en_mano(1, 1, db)
     
@@ -129,13 +137,53 @@ async def test_deshacer_movimiento():
     jugar_mov = switch_case(primera_figura, primer_id)
     print(f"la jugada que voy a hacer es : {jugar_mov}")
 
-    movimiento_jugado = juego_service.jugar_movimiento(1, 1, jugar_mov, db)
-    print(f"el tablero despues de jugar un moviento es de la forma : {obtener_fichas(1, db)}")
+    await juego_service.jugar_movimiento(1, 1, jugar_mov, db)
 
     fichas_actualizadas = juego_service.deshacer_movimiento(1, 1, db)
-    print(f"el tablero despues de deshacer el moviento es de la forma : {fichas_actualizadas}")
 
     assert tablero_inicial == fichas_actualizadas
-    
+    assert db.query(MovimientosParciales).count() == 0
 
+
+@pytest.mark.asyncio
+async def test_deshacer_movimientos(crear_session):
+
+    datos_partida = CrearPartida(  
+        nombre_host = "owner2",
+        nombre_partida = "partida_test_2",
+        cant_min_jugadores = 2,
+        cant_max_jugadores = 4
+     )
     
+    db = crear_session
+    
+    await partida_service.crear_partida(datos_partida, db)
+    await partida_service.unirse_partida("2", "j3", db)
+    await partida_service.unirse_partida("2", "j4", db)
+    partida_iniciada = await partida_service.iniciar_partida(2, 4, db)
+    
+    tablero_inicial = partida_iniciada["fichas"]
+    
+    id_movimientos_en_mano = obtener_id_movimientos_en_mano(2, 4, db)
+    
+    movimientos_en_mano = obtener_movimientos_en_mano(2, 4, db)
+    
+    for carta in range(3):
+        primer_id = id_movimientos_en_mano[carta]
+        print(f"el id de la carta de movimiento es : {primer_id}")
+
+        primera_figura = movimientos_en_mano[carta]
+        print(f"la figura relacionada al id : {primer_id} es : {primera_figura}")
+
+        jugar_mov = switch_case(primera_figura, primer_id)
+        print(f"la jugada que voy a hacer es : {jugar_mov}")
+
+        await juego_service.jugar_movimiento(2, 4, jugar_mov, db)
+
+    fichas_actualizadas = juego_service.deshacer_movimientos(2, 4, db)
+
+    assert tablero_inicial == fichas_actualizadas
+    assert db.query(MovimientosParciales).count() == 0
+
+
+        
