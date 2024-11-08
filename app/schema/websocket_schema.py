7953cff8aade28a5fbb59de1c4cad8fe2d5a9745
lucas <lucas.gallegos@mi.unc.edu.ar>
2024-11-08 15:00:38 -0300

    SCRUM-198 nuevo esquema de websocket para declarar como ganador a un jugador

--- app/schema/websocket_schema.py
+++ app/schema/websocket_schema.py
@@ -17,6 +17,7 @@ class WebSocketMessageType(str, Enum):
     FIGURA_DESCARTAR = "FiguraDescartar"
     REPOSICION_FIGURAS = "ReposicionFiguras"
     REPOSICION_MOVIMIENTOS = "ReposicionMovimientos"
+    PARTIDA_FINALIZADA = "PartidaFinalizada"
 
 class JugadorSchema(BaseModel):
     id: int
@@ -121,3 +122,11 @@ class ReposicionCartasFiguras(BaseModel):
 class ReposicionCartasMovimientos(BaseModel):
     type : Literal[WebSocketMessageType.REPOSICION_MOVIMIENTOS]
     cartas: List[dict]
+    
+class FinalizarPartidaDataSchema(BaseModel):
+    idGanador: int
+    nombreGanador: str
+
+class FinalizarPartidaSchema(BaseModel):
+    type: Literal[WebSocketMessageType.PARTIDA_FINALIZADA]
+    data: FinalizarPartidaDataSchema    
